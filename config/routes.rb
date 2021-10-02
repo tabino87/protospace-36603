f83488edd5a717b65ece4817a6f01a154d27dc45
tabino87 <ysdck809@yahoo.co.jp>
2021-10-02 16:37:40 +0900

    firstcommit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root to: "prototypes#index"
+  resources :prototypes do
+    resources :comments, only: :create
+    
+  end
+
+  resources :prototypes, only: [:new, :create, :show, :edit, :update, :destroy]
+
+  resources :users, only: :show
+end
